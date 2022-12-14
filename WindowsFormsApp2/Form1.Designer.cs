832ab2abb9b2797f9772e3071d65667730ecbb32
haaheen <giahien20032003@gmail.com>
2022-12-15 02:35:36 +0700

    hô hô hô

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,25 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BieuThuc = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tiento = new System.Windows.Forms.TextBox();
+            this.trungto = new System.Windows.Forms.TextBox();
+            this.hauto = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // BieuThuc
             // 
-            this.textBox1.Location = new System.Drawing.Point(22, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(755, 22);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.BieuThuc.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BieuThuc.Location = new System.Drawing.Point(22, 42);
+            this.BieuThuc.Name = "BieuThuc";
+            this.BieuThuc.Size = new System.Drawing.Size(755, 34);
+            this.BieuThuc.TabIndex = 0;
+            this.BieuThuc.Text = "(5+7)*3-4/2";
+            this.BieuThuc.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.button1.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(22, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(755, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseCompatibleTextRendering = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Dạng trung tố:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Dạng hậu tố:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dạng tiền tố:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(325, 30);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Nhập biểu thức toán học:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // tiento
+            // 
+            this.tiento.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tiento.Location = new System.Drawing.Point(22, 296);
+            this.tiento.Name = "tiento";
+            this.tiento.Size = new System.Drawing.Size(755, 34);
+            this.tiento.TabIndex = 13;
+            // 
+            // trungto
+            // 
+            this.trungto.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trungto.Location = new System.Drawing.Point(22, 199);
+            this.trungto.Name = "trungto";
+            this.trungto.Size = new System.Drawing.Size(755, 34);
+            this.trungto.TabIndex = 14;
+            // 
+            // hauto
+            // 
+            this.hauto.Font = new System.Drawing.Font("Cascadia Code SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hauto.Location = new System.Drawing.Point(22, 393);
+            this.hauto.Name = "hauto";
+            this.hauto.Size = new System.Drawing.Size(755, 34);
+            this.hauto.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.hauto);
+            this.Controls.Add(this.trungto);
+            this.Controls.Add(this.tiento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BieuThuc);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Đồ Án Expression Tree";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,7 +154,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox BieuThuc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tiento;
+        private System.Windows.Forms.TextBox trungto;
+        private System.Windows.Forms.TextBox hauto;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -21,5 +21,205 @@ namespace WindowsFormsApp2
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = BieuThuc.Text;
+            s = "(" + s;
+            s += ")";
+            tree root = build(s);
+            trungto.Text = inorder(root);
+            tiento.Text = preorder(root);
+            hauto.Text = postorder(root);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public class tree
+        {
+            public char data;
+            public tree left, right;
+        };
+
+        // Hàm tạo node mới
+        public static tree Node(char c)
+        {
+            tree n = new tree();
+            n.data = c;
+            n.left = n.right = null;
+            return n;
+        }
+
+        // Hàm check xem phần tử có phải toán tử hoặc dấu ngoặc hay không 
+        public static bool isOperator(char c)
+        {
+            if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '(' || c == ')') return true;
+            return false;
+        }
+
+        public static bool isDigit(char c)
+        {
+            if (c >= '0' && c <= '9') return true;
+            return false;
+        }
+
+        // Hàm tạo Cây biểu thức
+        static tree build(String s)
+        {
+
+            // Stack chứa các toán hạng (node stack) mỗi phần tử của node stack sẽ là một node trên Cây biểu thức
+            Stack<tree> stN = new Stack<tree>();
+
+            // Stack chứa các toán tử hoặc dấu ngoặc "(" (char stack)
+            Stack<char> stC = new Stack<char>();
+
+            // Tạo node mới chứa toán tử
+            tree N, NTren, NDuoi;
+
+            // Đặt độ ưu tiên của các toán tử
+            int[] p = new int[123];
+            p['+'] = p['-'] = 1;
+            p['/'] = p['*'] = 2;
+            p['^'] = 3;
+            p[')'] = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+
+                    // Push '(' vào char stack
+                    stC.Push(s[i]);
+                }
+
+                // Push toán hạng vào node stack
+                else if (isDigit(s[i]))
+                {
+                    N = Node(s[i]);
+                    stN.Push(N);
+                }
+                else if (p[s[i]] > 0)
+                {
+
+                    // Nếu 1 toán tử có độ ưu tiên thấp hơn hoặc bằng các toán tử trong char stack
+                    while (stC.Count != 0 && stC.Peek() != '('
+                        && ((s[i] != '^' && p[stC.Peek()] >= p[s[i]])
+                            || (s[i] == '^' && p[stC.Peek()] > p[s[i]])))
+                    {
+
+                        // Lấy toán tử ở đỉnh char stack và tạo ra 1 node mới cho cây biểu thức
+                        // Xóa toán tử ở đỉnh char stack
+                        N = Node(stC.Peek());
+                        stC.Pop();
+
+                        // Lấy phần tử ở đỉnh node stack và tạo ra 1 node mới cho cây biểu thức
+                        // Xóa phần tử ở đỉnh node stack
+                        NTren = stN.Peek();
+                        stN.Pop();
+
+                        // Lấy và xóa phần tử trên cùng kế tiếp từ node stack
+                        NDuoi = stN.Peek();
+                        stN.Pop();
+
+                        // Cập nhật cây
+                        N.left = NDuoi;
+                        N.right = NTren;
+
+                        // Push vào node stack
+                        stN.Push(N);
+                    }
+
+                    // Push s[i] vào char stack
+                    stC.Push(s[i]);
+                }
+                else if (s[i] == ')')
+                {
+
+                    while (stC.Count != 0 && stC.Peek() != '(')
+                    {
+                        // Tạo node mới t (node chứa toán tử)
+                        // Con phải là node ở đỉnh node stack, con trái là nút ngay dưới đỉnh node stack
+                        // Sau khi tạo xong, đưa t vào node stack
+
+                        N = Node(stC.Peek());
+                        stC.Pop();
+                        NTren = stN.Peek();
+                        stN.Pop();
+                        NDuoi = stN.Peek();
+                        stN.Pop();
+                        N.left = NDuoi;
+                        N.right = NTren;
+                        stN.Push(N);
+                    }
+                    // xóa dấu ngoặc "(" ra khỏi char stack
+                    stC.Pop();
+                }
+            }
+            NTren = stN.Peek();
+            return NTren;
+        }
+
+        // Hàm in ra post order traversal của cây
+        static string postorder(tree root)
+        {
+            string result = "";
+            if (root != null)
+            {
+                result += postorder(root.left).ToString() + postorder(root.right).ToString() + root.data.ToString();
+                postorder(root.left);
+                postorder(root.right);
+                Console.Write(root.data);
+            }
+            return result;
+        }
+
+        // Hàm in ra pre order traversal của cây
+        static string preorder(tree root)
+        {
+            string result = "";
+            if (root != null)
+            {
+                result += root.data.ToString() + preorder(root.left).ToString() + preorder(root.right).ToString();
+            }
+            return result;
+        }
+
+        static string inorder(tree root)
+        {
+            string result = "";
+            if (root != null)
+            {
+                result += inorder(root.left).ToString() + root.data.ToString() + inorder(root.right).ToString();
+            }
+            return result;
+        }
     }
 }
